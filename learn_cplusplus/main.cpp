bb31851aa277e0a950a6d7c01c8f3180abf7076d
susan <mwauragerald@njuguna-2.local>
2014-06-09 23:52:08 +0300

    Initial Commit

---
+++ learn_cplusplus/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  learn_cplusplus
+//
+//  Created by susan on 09/06/14.
+//  Copyright (c) 2014 nyumbani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
